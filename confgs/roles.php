515494c1dd5d31dbf9adad4b41a28c7719a6033d
Tap Nguyen <nghuytap@gmail.com>
2020-08-06 16:44:12 +0700

    s

--- confgs/roles.php
+++ confgs/roles.php
@@ -5,6 +5,9 @@ return array(
 	'category' => array('admin', 'create', 'update', 'delete'),
 	'newscategory' => array('admin', 'create', 'update', 'delete'),
 	'news' => array('admin', 'create', 'update', 'delete'),
+	'gallery' => array('admin', 'create', 'update', 'upload', 'update_order', 'deleteItem', 'view'),
+	'slideshow' => array('admin', 'create', 'delete', 'update', 'updateItem', 'view', 'upload','deleteItem'),
+	'settings' => array('admin','create','update', 'delete'),
 	'user' => array('admin', 'create', 'update', 'delete'),
 	'roles' => array('admin', 'create', 'update', 'delete'),
 )
