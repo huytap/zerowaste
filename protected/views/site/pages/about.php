aa2380099015656932285ca92c677049c217fa63
Tap Nguyen <nghuytap@gmail.com>
2020-11-09 12:05:57 +0700

    s

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -1,38 +1,43 @@
 <div class="wrapper about">
   <div class="header">
       <?php $this->widget('MenuWidget');?>
-    <div class="banner hidden-xs"><img src="images/banner-about.png?v=002" class="img-responsive"></div>
+    <div class="banner hidden-sm hidden-xs"><img src="images/banner-about.png?v=002" class="img-responsive"></div>
+    <div class="banner hidden-lg hidden-md hidden-xs"><img src="images/banner-about-ipad.png?v=002" class="img-responsive"></div>
   </div>
   <div class="banner hidden-lg hidden-md hidden-sm"><img src="images/banner-about-m.png?v=2" class="img-responsive"></div>
   <div class="container about-content">
     <div class="row">
-      <div class="col-md-6 hidden-xs">
+      <div class="col-md-6 hidden-xs hidden-sm">
         <div class="about-maybay"><img src="images/about-maybay.png" class="img-responsive"></div>
         <div class="about-icon"><img src="images/about-icon.png" class="img-responsive"></div>
       </div>
-      <div class="col-md-5">
+	 <div class="col-sm-5 hidden-xs hidden-md hidden-lg">
+        <div class="about-maybay"><img src="images/about-maybay-ipad.png" class="img-responsive"></div>
+      </div>
+      <div class="col-md-5 ip">
         <p>Để giảm rác thải nhựa tại Việt Nam, việc lan tỏa lối sống Zero Waste - Không Rác Thải Nhựa là một điều hết sức cần thiết. Làm cách nào để sản phẩm, dịch vụ zero-waste đến gần hơn với những ai có nhu cầu?</p>
 
         <p>Nằm trên nền tảng web, Zero Waste Wonderland là cổng thông tin hoàn thiện đầu tiên về xu hướng sống zero-waste. Mục tiêu của Zero Waste Wonderland là làm cầu nối để đẩy mạnh và lan rộng các hoạt động zero-waste tại Việt Nam.</p>
       </div>
       <div class="col-md-1 hidden-xs">&nbsp;</div>
     </div>
+    <div class="about-icon hidden-lg hidden-md hidden-xs"><img src="images/about-icon-ipad.png" class="img-responsive"></div>
     <div class="row hidden-xs">
-      <div class="col-md-5" style="padding-top: 100px;">
+      <div class="col-md-5 col-sm-7 ipad" style="padding-top: 100px;">
         <p>Đến với Zero Waste Wonderland, bạn có thể thỏa thích "tham quan" các địa điểm zero-waste, tìm hiểu bộ sưu tập sản phẩm zero-waste độc đáo. Ngoài ra, trang của chúng mình cũng sẽ cập nhật các thông tin mới nhất về môi trường, đặc biệt là các sự kiện zero-waste để các bạn tiện theo dõi!</p>
         <p>Nào, hãy dạo một vòng Zero Waste Wonderland. Bạn sẽ thấy lối sống zero-waste không khó chút nào mà vừa vui lại vừa dễ.</p>
       </div>
       <div class="col-md-1">&nbsp;</div>
-      <div class="col-md-5">
+      <div class="col-md-5 col-sm-5">
         <div class="about-zwww"><img src="images/about-zww.png" class="img-responsive"></div>
       </div>
     </div>
-		<div class="duck hidden-xs">
-			<img src="<?php echo Yii::app()->baseUrl?>/images/duck.png" class="img-responsive"/>
-		</div>
+	<div class="duck  hidden-sm hidden-xs">
+		<img src="<?php echo Yii::app()->baseUrl?>/images/duck.png" class="img-responsive"/>
+	</div>
   </div>
-  <div class="about-zwww hidden-lg hidden-md"><img src="images/about-zww-m.png?v=001" class="img-responsive"></div>
-  <div class="container about-content2 hidden-lg hidden-md">
+  <div class="about-zwww hidden-lg hidden-md hidden-sm"><img src="images/about-zww-m.png?v=001" class="img-responsive"></div>
+  <div class="container about-content2 hidden-lg hidden-md hidden-sm">
     <p>Đến với Zero Waste Wonderland, bạn có thể thỏa thích "tham quan" các địa điểm zero-waste, tìm hiểu bộ sưu tập sản phẩm zero-waste độc đáo. Ngoài ra, trang của chúng mình cũng sẽ cập nhật các thông tin mới nhất về môi trường, đặc biệt là các sự kiện zero-waste để các bạn tiện theo dõi!</p>
     <p>Nào, hãy dạo một vòng Zero Waste Wonderland. Bạn sẽ thấy lối sống zero-waste không khó chút nào mà vừa vui lại vừa dễ.</p>
   </div>
